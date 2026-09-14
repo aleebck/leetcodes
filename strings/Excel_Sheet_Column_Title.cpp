a6eeff39d3fbef9828bc63b637b6c5279ecfaaa5
aleebck <alibekmakhkamow@gmail.com>
2026-09-14 01:44:55 -0500

    Add Excel sheet column title solution

---
+++ strings/Excel_Sheet_Column_Title.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string res;
+        while(columnNumber >0)
+        {
+            columnNumber--;
+
+            res.push_back('A' + columnNumber%26);
+
+            columnNumber /=26;
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
