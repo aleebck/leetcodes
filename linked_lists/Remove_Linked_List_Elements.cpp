25a1bae23b8c0da513197fe83f82382268106a6f
aleebck <alibekmakhkamow@gmail.com>
2026-09-16 01:44:06 -0500

    Add Remove linked list elements solution

---
+++ linked_lists/Remove_Linked_List_Elements.cpp
@@ -0,0 +1,31 @@
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        while(head && head->val == val)
+        {
+            head = head->next;
+        }
+        ListNode* current = head;
+        while(current && current-> next)
+        {
+            if(current->next->val == val)
+            {
+                current->next = current->next->next;
+            }
+            else{
+                current = current->next;
+            }
+        }
+        return head;
+    }
+};
